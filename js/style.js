47a7b2cd668a9b7a6fc02a6e3f1052d40f653684
atm0016 <atm0016@example.com>
2017-10-04 18:56:05 +0530

    Created menu of responsive

--- js/style.js
+++ js/style.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
   // 検索ボックスを閉じる
   $("#close").click(function(){
-    $("#sidebar-search-box").hide();
+    $("#sidebar-search-box").collapse("hide");
   });
   $("#search-btn").click(function(){
-    $("#sidebar-search-box").show();
+    $("#sidebar-search-box").collapse("show");
+  });
+
+  // メニュー閉じる
+  $("#menu-close").click(function(){
+    $(".reponsive-menu-container").collapse("hide");
+  });
+  $("$menu-btn").click(function(){
+    $(".reponsive-menu-container").collapse("show");
   });
 });
